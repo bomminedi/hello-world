b9949448e38e357af03f79cebdc820f480c729de
Swaraj <Swaraj@15.com>
2018-01-14 20:49:49 +0530

    jiji

--- TrainingSerious/src/Base/Demo.java
+++ TrainingSerious/src/Base/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo");
-		System.out.println("Dem5");
+		System.out.println("Dem3");
 	}
 
 }
