ca7ac261c7c21a7dc53347d9e5ec48e5d6c598ab
bomminedi.swaraj@gmail.com <bomminedi.swaraj@gmail.com>
2018-01-15 03:57:40 -0800

    Added

---
+++ hello-world-master/TrainingSerious/src/Base/Demo.java
@@ -0,0 +1,11 @@
+package Base;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo");
+		System.out.println("Dem1.332");
+	}
+
+}
