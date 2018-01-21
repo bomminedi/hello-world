1a60e3109445cd35e940a63ecbd9af3635603c4e
bomminedi.swaraj@gmail.com <bomminedi.swaraj@gmail.com>
2018-01-20 20:50:28 -0800

    updated

--- TrainingSerious/src/Base/Demo.java
+++ TrainingSerious/src/Base/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo");
-		System.out.println("Dem12");
+		System.out.println("Dem123");
 	}
 
 }
